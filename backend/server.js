332419ca30d92416b9f73bced7997f33106bf631
BANI GIT <bani.rezaie@gmail.com>
2020-06-27 18:54:20 +0100

    back-end server

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,60 @@
+//--------------------------------------------
+//first //if you don't use React, you need to make a package.json:
+//npm init -y
+//--------------------------------------------
+//then //you need to install express:
+//npm i express
+//then // you need to define it in index.js like this exactly:
 const express = require("express");
-//npm install express
-// and also
-//npm install uuid
-
 const app = express();
+//-----------------------------------------------
+//then //if you don't use React you need to define these in your index.js as well:
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+//-----------------------------------------------
+// and also
+//npm install uuid (for node)
 
+const path = require("path");
+const members = require("../src/members");
+const moment = require("moment");
 
-app.get("/", () => {});
+const logger = (req, res, next) => {
+  console.log(
+    `${req.protocol}://${req.get("host")}${
+      req.originalUlr
+    }:${moment().format()}`
+  );
+  next();
+};
+
+app.get("/api/members", (req, res) => {
+  res.send(members);
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!!!</h1>");
+  //res.json
+  //res.render()
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 //app.post()
 //app.delete()
 //app.put()
 //,ect
 
-app.listen(4000, () => {
-  console.log("it is on port 4000");
+//-------
+/* you can also have nodemon:
+npm i -D nodemon
+and you can see it in dev dependencies in package.json
+then in the scripts: you need :
+start: "node index"
+dev : "nodemon index"
+to have automatic server update
+*/
+//-------
+app.listen(5000, () => {
+  console.log("it is on port 5000");
   //fetch from database
   //Load pages
   //return JSON
---
+++ src/members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: "abcd",
+    age: 22,
+    status: "active",
+  },
+  {
+    id: 2,
+    name: "efgh",
+    age: 25,
+    status: "de-active",
+  },
+  {
+    id: 3,
+    name: "ijkl",
+    age: 29,
+    status: "super-active",
+  },
+];
+
+module.exports = members;
